5ec0196adbf0d453e98b37d036e4ff51aa991a18
fennecdjay <astor.jeremie@wanadoo.fr>
2017-01-26 18:13:31 +0100

    add help command.

--- zpm.c
+++ zpm.c
@@ -277,9 +277,15 @@ int plugins_update_local_clone() {
     return ret;
 }
 
+void usage() {
+    printf("%s\n", "Usage:\n\tzpm 'zsh-users/zsh-syntax-highlighting'");
+    printf("%s\n", "\nAvailable commands:\n\tzpm reset\n\tzpm list");
+    printf("%s\n", "\tzpm update\n\tzpm help");
+}
+
 int main(int argc, char* argv[]) {
     if (argc <= 1) {
-        printf("%s\n", "Usage:\n\tzpm 'zsh-users/zsh-syntax-highlighting'");
+		usage();
         return 1;
     }
 
@@ -304,7 +310,9 @@ int main(int argc, char* argv[]) {
         printf("%s\n", plugin_list);
         free(plugin_list);
         return 0;
-
+	} else if (strstr(plugin_name_or_command, "help")) {
+        usage();
+ 		return 0;
     } else {
         plugin_name = malloc(PATH_MAX);
         strcpy(plugin_name, plugin_name_or_command);
