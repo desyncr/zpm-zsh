b9b46655a7dce818896d0061e77cd94894c64b6b
fennecdjay <astor.jeremie@wanadoo.fr>
2017-01-31 00:35:50 +0100

    Fix rmdir_r argument and indent.

--- zpm.c
+++ zpm.c
@@ -218,50 +218,50 @@ int mkdir_p(const char *path) {
 }
 
 int rmdir_r(const char *path) {
-   DIR *d = opendir(path);
-   size_t path_len = strlen(path);
-   int r = -1;
+    DIR *d = opendir(path);
+    size_t path_len = strlen(path);
+    int r = -1;
 
-   if (d) {
-      struct dirent *p;
+    if (d) {
+        struct dirent *p;
 
-      r = 0;
+        r = 0;
 
-      while (!r && (p=readdir(d))) {
-          int r2 = -1;
-          char *buf;
-          size_t len;
+        while (!r && (p=readdir(d))) {
+            int r2 = -1;
+            char *buf;
+            size_t len;
 
-          if (!strcmp(p->d_name, ".") || !strcmp(p->d_name, "..")) {
-             continue;
-          }
+            if (!strcmp(p->d_name, ".") || !strcmp(p->d_name, "..")) {
+                continue;
+            }
 
-          len = path_len + strlen(p->d_name) + 2;
-          buf = malloc(len);
+            len = path_len + strlen(p->d_name) + 2;
+            buf = malloc(len);
 
-          if (buf) {
-             struct stat statbuf;
+            if (buf) {
+                struct stat statbuf;
 
-             snprintf(buf, len, "%s/%s", path, p->d_name);
+                snprintf(buf, len, "%s/%s", path, p->d_name);
 
-             if (!stat(buf, &statbuf)) {
-                if (S_ISDIR(statbuf.st_mode)) {
-                   r2 = rmdir_r(buf);
-                } else {
-                   r2 = unlink(buf);
+                if (!stat(buf, &statbuf)) {
+                    if (S_ISDIR(statbuf.st_mode)) {
+                       r2 = rmdir_r(buf);
+                    } else {
+                        r2 = unlink(buf);
+                    }
                 }
-             }
-             free(buf);
-          }
-          r = r2;
-      }
-      closedir(d);
-   }
-   if (!r) {
-      r = rmdir(path);
-   }
+                free(buf);
+            }
+            r = r2;
+        }
+        closedir(d);
+    }
+    if (!r) {
+        r = rmdir(path);
+    }
 
-   return r;
+    return r;
 }
 
 int plugin_remove_entry(char* plugin_name, char* file_name) {
@@ -306,7 +306,7 @@ int plugin_remove(char* plugin_name, int uninstall) {
       strcpy(plugin_path, getenv("HOME"));
       strcat(plugin_path, "/.zpm/plugins/");
       strcat(plugin_path, plugin_name);
-      rmdir_r(plugin_name);
+      rmdir_r(plugin_path);
     }
     return 0;
 }
